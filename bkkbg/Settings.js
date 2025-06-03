38e781df5cfd41b69147b4ec836ddf990295fb72
Modiholodri <reinhold.lauer@gmail.com>
2025-06-03 13:31:14 +0700

    Split the Settings handling

---
+++ bkkbg/Settings.js
@@ -0,0 +1,73 @@
+function loadToggleSetting(elementName) {
+    const toggleState = localStorage.getItem(elementName + 'State') || 'expanded';
+    const element = document.getElementById(elementName);
+    toggleState === 'expanded' ? element.classList.add('show') : element.classList.remove('show');
+}
+
+// Load settings from localStorage
+function loadSettings() {
+    loadToggleSetting('matchReportForm');
+    loadToggleSetting('ratingList');
+    loadToggleSetting('rankingLists');
+
+    // Ranking List Selection
+    const rankingListSelection = localStorage.getItem('rankingListSelection') || 'matchesPlayed';
+    document.getElementById('rankingListSelection').value = rankingListSelection;
+
+    // Player Name Selection
+    const yourName = localStorage.getItem('yourName') || '';
+    document.getElementById('yourName').value = yourName;
+
+    // Expand/Collapse the Player Name Selection depending on the Ranking List Selection
+    const playerNameSelection = document.getElementById("playerNameSelection");
+    playerNameSelection.style.display = rankingListSelection === "playerInfo" ? "flex" : "none";
+
+    // Expand/Collapse the Replay section depending on the Ranking List Selection
+    const replaySection = document.getElementById("replaySection");
+    replaySection.style.display = rankingListSelection === "ratingList" ? "flex" : "none"; // Hide the element
+
+    // Interval Selection
+    const intervalSelection = localStorage.getItem('intervalSelection') || 'Daily';
+    document.getElementById('intervalSelection').value = intervalSelection;
+
+    loadToggleSetting('rankingChart');
+    loadToggleSetting('rankingSummary');
+    loadToggleSetting('matchList');
+}
+
+// load the settings once the DOM is ready
+document.addEventListener('DOMContentLoaded', loadSettings);
+
+
+function saveToggleSetting(elementName) {
+    const matchReportFormCollapsed = document.getElementById(elementName).classList.contains('show') ? 'expanded' : 'collapsed';
+    localStorage.setItem(elementName + 'State', matchReportFormCollapsed);
+}
+
+function saveValueSetting(elementName) {
+    const rankingListSelection = document.getElementById(elementName).value;
+    localStorage.setItem(elementName, rankingListSelection);
+}
+
+// Save settings to localStorage
+function saveSettings() {
+    saveToggleSetting('matchReportForm');
+    saveToggleSetting('ratingList');
+    saveToggleSetting('rankingLists');
+
+    saveValueSetting('rankingListSelection');
+    saveValueSetting('yourName');
+    saveValueSetting('intervalSelection');
+
+    saveToggleSetting('rankingChart');
+    saveToggleSetting('rankingSummary');
+    saveToggleSetting('matchList');
+}
+
+// Save the settings when the button is clicked
+document.getElementById('saveSettingsButton').addEventListener('click', () => {
+    saveSettings();
+});
+
+// Save settings to localStorage before the page is unloaded
+window.addEventListener('beforeunload', saveSettings);
