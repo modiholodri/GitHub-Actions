baf7a3072530b71f55b8bc9736ec135f8406e691
Modiholodri <reinhold.lauer@gmail.com>
2025-10-05 14:50:16 +0700

    Add Start Tournament button and implement first draft version

--- bkkbg/Tournament.js
+++ bkkbg/Tournament.js
@@ -32,3 +32,48 @@ function generateRoundRobinTournament(selectedPlayers) {
 
     document.getElementById('tournament').innerHTML = html;
 }
+
+// Start a new tournament
+document.getElementById('tournamentManagement').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    // Tournament HTML
+    let tournamentHTML = document.getElementById('tournament').innerHTML;
+
+    alert('Starting a new tournament!\n' + tournamentHTML);
+    
+    const repoName = document.getElementById('clubSelection').value;
+
+    await refreshRunsStatus();
+    setSubmissionStatus(`Starting tournament...`);
+    setRunsInfo('Hold on a sec...');
+    previousRunID = latestRunID;
+    if ( latestRunStatus === 'Submitting' || latestRunStatus === 'Queued' || latestRunStatus === 'In Progress') {
+        setSubmissionStatus('Another submission in progress.\nTry again in a few seconds...');
+        anotherSubmissionActive = true;
+        // document.getElementById("start_tournament").disabled = true;
+    }
+    else try {
+        const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/dispatches`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `token ${githubToken}`,
+                'Content-Type': 'application/json',
+                'Accept': 'application/vnd.github.v3+json',
+            },
+            body: JSON.stringify({ 
+                event_type: 'start_tournament', 
+                client_payload: { 
+                    tournament_html: `${tournamentHTML}`,
+                } 
+            })
+        });
+
+        newSubmission = true;
+        // document.getElementById("submit").disabled = true;
+    } 
+    catch (error) { 
+        // Error triggering GitHub Action: Failed to execute 'json' on 'Response': Unexpected end of JSON input
+        alert('Error triggering GitHub Action: ' + error.message); // Handle error (e.g., notify user, retry, etc.)
+    }
+});
