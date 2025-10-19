4809dfd4e9f91eb5309cc43e541cca174f965e91
Modiholodri <reinhold.lauer@gmail.com>
2025-10-19 10:28:20 +0700

    Move supporting functions down #244

--- bkkbg/Tournament.js
+++ bkkbg/Tournament.js
@@ -1,109 +1,25 @@
 let tournamentGenerated = false;
 
-function make8PlayersDoubleElimination() {
-    let html = '<h5>Double Elimination</h5>\n';
-    // Main Bracket
-    html += '<h5>Main - 5 points</h5>\n';
-    html += `<p>\n`;
-    html += `Player~1~ #1# Player~8~<br>\n`;
-    html += `Player~4~ #2# Player~5~<br>\n`;
-    html += `Player~3~ #3# Player~6~<br>\n`;
-    html += `Player~2~ #4# Player~7~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Winner~1~ #7# Winner~2~<br>\n`;
-    html += `Winner~3~ #8# Winner~4~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Winner~7~ #11# Winner~8~<br>\n`;
-    html += `</p>\n`;
-    // Consolation Bracket
-    html += '<h5>Consolation - 3 points</h5>\n';
-    html += `<p>\n`;
-    html += `Loser~1~ #5# Loser~2~<br>\n`;
-    html += `Loser~3~ #6# Loser~4~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Loser~8~ #9# Winner~5~<br>\n`;
-    html += `Loser~7~ #10# Winner~6~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Winner~9~ #12# Winner~10~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Loser~11~ #13# Winner~12~<br>\n`;
-    html += `</p>\n`;
-    // Final
-    html += '<h5>Final - 5 points</h5>\n';
-    html += `<p>\n`;
-    html += `Winner~11~ #14# Winner~13~<br>\n`;
-    html += `</p>\n`;
-    return html;
-}
-
-function make16PlayersDoubleElimination() {
-    let html = '<h5>Double Elimination</h5>\n';
-    // Main Bracket
-    html += '<h5>Main - 5 points</h5>\n';
-    html += `<p>\n`;
-    html += `Player~1~ #1# Player~16~<br>\n`;
-    html += `Player~8~ #3# Player~9~<br>\n`;
-    html += `Player~4~ #2# Player~13~<br>\n`;
-    html += `Player~12~ #4# Player~5~<br>\n`;
-    html += `Player~2~ #5# Player~15~<br>\n`;
-    html += `Player~10~ #6# Player~7~<br>\n`;
-    html += `Player~3~ #7# Player~14~<br>\n`;
-    html += `Player~11~ #8# Player~6~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Winner~1~ #13# Winner~2~<br>\n`;
-    html += `Winner~3~ #14# Winner~4~<br>\n`;
-    html += `Winner~5~ #15# Winner~6~<br>\n`;
-    html += `Winner~7~ #16# Winner~8~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Winner~13~ #21# Winner~14~<br>\n`;
-    html += `Winner~15~ #22# Winner~16~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Winner~21~ #25# Winner~22~<br>\n`;
-    html += `</p>\n`;
-    // Consolation Bracket
-    html += '<h5>Consolation - 3 points</h5>\n';
-    html += `<p>\n`;
-    html += `Loser~1~ #9# Loser~2~<br>\n`;
-    html += `Loser~3~ #10# Loser~4~<br>\n`;
-    html += `Loser~5~ #11# Loser~6~<br>\n`;
-    html += `Loser~7~ #12# Loser~8~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Loser~16~ #17# Winner~9~<br>\n`;
-    html += `Loser~15~ #18# Winner~10~<br>\n`;
-    html += `Loser~14~ #19# Winner~11~<br>\n`;
-    html += `Loser~13~ #20# Winner~12~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Winner~17~ #23# Winner~18~<br>\n`;
-    html += `Winner~19~ #24# Winner~20~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Loser~22~ #26# Winner~23~<br>\n`;
-    html += `Winner~24~ #27# Loser~21~<br>\n`;
-    html += `</p><p>\n`;
-    html += `Loser~27~ #29# Winner~28~<br>\n`;
-    html += `</p>\n`;
-    // Final
-    html += '<h5>Final - 5 points</h5>\n';
-    html += `<p>\n`;
-    html += `Winner~25~ #30# Winner~29~<br>\n`;
-    html += `</p>\n`;
-    return html;
-}
-
 function generateDoubleElimination(selectedPlayers) {
-    const players = [...selectedPlayers].sort(() => generator.random() - 0.5);
     const today = new Date().toISOString().slice(0, 10);
     const byTournamentDirector = 'by ' + document.getElementById('yourName').value.trim();
-    
     let html = `<p id="today" style="text-align: center">${today} ${byTournamentDirector}</p>\n`;
-    html += make16PlayersDoubleElimination();
 
+    const numPlayers = selectedPlayers.length;
+    if (numPlayers <= 8)
+        html += make8PlayersDoubleElimination();
+    else
+        html += make16PlayersDoubleElimination();
+
+    const players = [...selectedPlayers].sort(() => generator.random() - 0.5);
     // fill in the players
     let i = 0;
     while (i < players.length) {
         html = html.replace(`Player~${i + 1}~`, players[i]);
         i++;
     }
-    while (i < 16) {
+    // fill the rest with Byes
+    while (i < numPlayers) {
         html = html.replace(`Player~${i + 1}~`, 'Bye');
         i++;
     }
@@ -373,6 +289,95 @@ function highlightTodaysMatches() {
     tournamentDiv.innerHTML = html;
 }
 
+function make8PlayersDoubleElimination() {
+    let html = '<h5>Double Elimination</h5>\n';
+    // Main Bracket
+    html += '<h5>Main - 5 points</h5>\n';
+    html += `<p>\n`;
+    html += `Player~1~ #1# Player~8~<br>\n`;
+    html += `Player~4~ #2# Player~5~<br>\n`;
+    html += `Player~3~ #3# Player~6~<br>\n`;
+    html += `Player~2~ #4# Player~7~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Winner~1~ #7# Winner~2~<br>\n`;
+    html += `Winner~3~ #8# Winner~4~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Winner~7~ #11# Winner~8~<br>\n`;
+    html += `</p>\n`;
+    // Consolation Bracket
+    html += '<h5>Consolation - 3 points</h5>\n';
+    html += `<p>\n`;
+    html += `Loser~1~ #5# Loser~2~<br>\n`;
+    html += `Loser~3~ #6# Loser~4~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Loser~8~ #9# Winner~5~<br>\n`;
+    html += `Loser~7~ #10# Winner~6~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Winner~9~ #12# Winner~10~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Loser~11~ #13# Winner~12~<br>\n`;
+    html += `</p>\n`;
+    // Final
+    html += '<h5>Final - 5 points</h5>\n';
+    html += `<p>\n`;
+    html += `Winner~11~ #14# Winner~13~<br>\n`;
+    html += `</p>\n`;
+    return html;
+}
+
+function make16PlayersDoubleElimination() {
+    let html = '<h5>Double Elimination</h5>\n';
+    // Main Bracket
+    html += '<h5>Main - 5 points</h5>\n';
+    html += `<p>\n`;
+    html += `Player~1~ #1# Player~16~<br>\n`;
+    html += `Player~8~ #3# Player~9~<br>\n`;
+    html += `Player~4~ #2# Player~13~<br>\n`;
+    html += `Player~12~ #4# Player~5~<br>\n`;
+    html += `Player~2~ #5# Player~15~<br>\n`;
+    html += `Player~10~ #6# Player~7~<br>\n`;
+    html += `Player~3~ #7# Player~14~<br>\n`;
+    html += `Player~11~ #8# Player~6~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Winner~1~ #13# Winner~2~<br>\n`;
+    html += `Winner~3~ #14# Winner~4~<br>\n`;
+    html += `Winner~5~ #15# Winner~6~<br>\n`;
+    html += `Winner~7~ #16# Winner~8~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Winner~13~ #21# Winner~14~<br>\n`;
+    html += `Winner~15~ #22# Winner~16~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Winner~21~ #25# Winner~22~<br>\n`;
+    html += `</p>\n`;
+    // Consolation Bracket
+    html += '<h5>Consolation - 3 points</h5>\n';
+    html += `<p>\n`;
+    html += `Loser~1~ #9# Loser~2~<br>\n`;
+    html += `Loser~3~ #10# Loser~4~<br>\n`;
+    html += `Loser~5~ #11# Loser~6~<br>\n`;
+    html += `Loser~7~ #12# Loser~8~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Loser~16~ #17# Winner~9~<br>\n`;
+    html += `Loser~15~ #18# Winner~10~<br>\n`;
+    html += `Loser~14~ #19# Winner~11~<br>\n`;
+    html += `Loser~13~ #20# Winner~12~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Winner~17~ #23# Winner~18~<br>\n`;
+    html += `Winner~19~ #24# Winner~20~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Loser~22~ #26# Winner~23~<br>\n`;
+    html += `Winner~24~ #27# Loser~21~<br>\n`;
+    html += `</p><p>\n`;
+    html += `Loser~27~ #29# Winner~28~<br>\n`;
+    html += `</p>\n`;
+    // Final
+    html += '<h5>Final - 5 points</h5>\n';
+    html += `<p>\n`;
+    html += `Winner~25~ #30# Winner~29~<br>\n`;
+    html += `</p>\n`;
+    return html;
+}
+
 async function uploadTournament() {
     // Remove blue background spans before uploading
     let tournamentDiv = document.createElement('div');
@@ -381,15 +386,9 @@ async function uploadTournament() {
     tournamentDiv.querySelectorAll('span[style="background-color: blue;"]').forEach(span => {
         span.replaceWith(document.createTextNode(span.textContent));
     });
-    // Remove all tables
-    // tournamentDiv.querySelectorAll('table').forEach(table => {
-    //     table.remove();
-    // });
 
     let tournamentHTML = tournamentDiv.innerHTML;
 
-    // alert('Starting a new tournament!\n' + tournamentHTML);
-    
     const repoName = document.getElementById('clubSelection').value;
 
     await refreshRunsStatus();
